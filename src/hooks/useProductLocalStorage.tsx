e0e8870326f02d7769122c9d35d1c971771a5817
jovan <jovanus@yahoo.co.id>
2024-08-15 21:33:45 +0700

    refactor: extend uidotdev useLocalStorage hooks

--- src/hooks/useProductLocalStorage.tsx
+++ src/hooks/useProductLocalStorage.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 import {
   ProductFragment,
   ProductVariantFragment,
 } from "@/__generated__/graphql";
-import { useIsClient } from "@uidotdev/usehooks";
+import { useIsClient, useLocalStorage } from "@uidotdev/usehooks";
 
 import { LOCAL_STORAGE_KEYS } from "@/lib/constant";
 
@@ -14,38 +14,23 @@ interface SelectedValue {
 
 export function useProductLocalStorage() {
   const isClient = useIsClient();
-  const [settings, setSettings] = useState<SelectedValue>();
-  const [stone, setStone] = useState<SelectedValue>();
+  const [stone, setStone] = useLocalStorage<SelectedValue | null>(
+    LOCAL_STORAGE_KEYS.Stone,
+    null,
+  );
+  const [settings, setSettings] = useLocalStorage<SelectedValue | null>(
+    LOCAL_STORAGE_KEYS.Setting,
+    null,
+  );
 
-  useEffect(() => {
-    const setting = localStorage.getItem(LOCAL_STORAGE_KEYS.Setting);
-    const stone = localStorage.getItem(LOCAL_STORAGE_KEYS.Stone);
-
-    setting && setSettings(JSON.parse(setting));
-    stone && setStone(JSON.parse(stone));
-  }, [
-    localStorage.getItem(LOCAL_STORAGE_KEYS.Setting),
-    localStorage.getItem(LOCAL_STORAGE_KEYS.Stone),
-  ]);
-
-  function setStoneToLocalStorage(args: SelectedValue) {
-    localStorage.setItem(LOCAL_STORAGE_KEYS.Stone, JSON.stringify(args));
-  }
-
-  function deleteStoneFromLocalStorage() {
-    localStorage.removeItem(LOCAL_STORAGE_KEYS.Stone);
-  }
-
-  const value = useMemo(
+  return useMemo(
     () => ({
       settings,
       stone,
       loading: !isClient,
-      setStoneToLocalStorage,
-      deleteStoneFromLocalStorage,
+      setStone,
+      setSettings,
     }),
     [settings, stone, isClient],
   );
-
-  return value;
 }
