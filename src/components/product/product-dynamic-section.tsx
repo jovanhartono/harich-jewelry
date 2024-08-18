ab1d6ad60daa92e48a4c83289da2294c027e0847
jovan <jovanus@yahoo.co.id>
2024-08-18 14:42:15 +0700

    feat: size and engraving on product page with type setting

--- src/components/product/product-dynamic-section.tsx
+++ src/components/product/product-dynamic-section.tsx
@@ -1,12 +1,8 @@
 "use client";
 
-import { memo, useCallback, useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 import NextLink from "next/link";
-import {
-  AttributeInput,
-  CartLineInput,
-  ProductFragment,
-} from "@/__generated__/graphql";
+import { AttributeInput, CartLineInput } from "@/__generated__/graphql";
 import { useProduct } from "@/app/product/provider";
 import {
   StoneModalProvider,
@@ -19,36 +15,40 @@ import { GemIcon } from "lucide-react";
 import AddToCart from "@/components/cart/add-to-cart";
 import { Price } from "@/components/product/price";
 import { ProductCTA } from "@/components/product/product-cta";
-import { PRODUCT_TYPES } from "@/lib/constant";
+import { StoneCertificate } from "@/components/product/stone-certificate";
+import { StoneSpecifications } from "@/components/product/stone/stone-specifications";
+import { CART_LINE_ATTRIBUTE_KEYS, PRODUCT_TYPES } from "@/lib/constant";
 import { useProductLocalStorage } from "@/hooks/useProductLocalStorage";
 
-const SettingsSection = memo(function SettingsSection() {
+function SettingsSection() {
+  const [engraving, setEngraving] = useState<string>();
+  const [size, setSize] = useState<string>();
   const { product, selectedVariant } = useProduct();
   const { stone, setStone } = useProductLocalStorage();
   const { open } = useStoneModal();
 
   const openStoneModal = useCallback(
-    () => open(product.shape.value),
-    [open, product.shape.value],
+    () => open(product?.shape?.value),
+    [open, product?.shape?.value],
   );
 
   const lines: CartLineInput[] = useMemo(() => {
     if (stone && selectedVariant) {
       const settingAttributes: AttributeInput[] = [];
 
-      // if (engraving) {
-      //     settingAttributes.push({
-      //         key: CART_LINE_ATTRIBUTE_KEYS.ENGRAVING,
-      //         value: engraving,
-      //     });
-      // }
-      //
-      // if (size) {
-      //     settingAttributes.push({
-      //         key: CART_LINE_ATTRIBUTE_KEYS.SIZE,
-      //         value: size,
-      //     });
-      // }
+      if (engraving) {
+        settingAttributes.push({
+          key: CART_LINE_ATTRIBUTE_KEYS.ENGRAVING,
+          value: engraving,
+        });
+      }
+
+      if (size) {
+        settingAttributes.push({
+          key: CART_LINE_ATTRIBUTE_KEYS.SIZE,
+          value: size,
+        });
+      }
 
       return [
         {
@@ -64,7 +64,7 @@ const SettingsSection = memo(function SettingsSection() {
     }
 
     return [];
-  }, [selectedVariant, stone]);
+  }, [engraving, selectedVariant, size, stone]);
 
   if (!stone) {
     return (
@@ -89,7 +89,7 @@ const SettingsSection = memo(function SettingsSection() {
         <h2>Your Diamond Selection</h2>
         <GemIcon className="ml-2 size-4 text-default-600" />
       </div>
-      <div className="mb-6 mt-1.5 flex items-stretch gap-3 rounded-large bg-default-50 p-3">
+      <div className="mb-6 mt-1.5 flex items-stretch gap-3 rounded-large bg-default-100 p-3">
         <NextLink href={`/product/${stone.product.handle}`}>
           <Image
             className="aspect-square w-20 shrink-0 object-cover object-center lg:w-28"
@@ -105,7 +105,7 @@ const SettingsSection = memo(function SettingsSection() {
             {stone.product.title}
           </NextLink>
           <Price
-            className="mt-2 font-sans font-normal"
+            className="mt-2 text-sm font-normal"
             compareAtPrice={
               stone.product.compareAtPriceRange.maxVariantPrice.amount
             }
@@ -129,14 +129,50 @@ const SettingsSection = memo(function SettingsSection() {
           </div>
         </div>
       </div>
-      <AddToCart lines={lines} />
+
+      <div className="mb-6 flex gap-3">
+        <label htmlFor="engraving" className="flex flex-col gap-1">
+          <span className="text-sm font-medium">Engraving</span>
+          <input
+            onChange={(e) => setEngraving(e.target.value)}
+            type="text"
+            id="engraving"
+            className="border border-default-700 p-3 outline-none focus:ring-black"
+          />
+        </label>
+        <label htmlFor="size" className="flex flex-col gap-1">
+          <span className="text-sm font-medium">Size</span>
+          <input
+            onChange={(e) => setSize(e.target.value)}
+            type="text"
+            id="size"
+            className="border border-default-700 p-3 outline-none focus:ring-black"
+          />
+        </label>
+      </div>
+
+      <AddToCart lines={lines}>Complete Setup</AddToCart>
     </section>
   );
-});
+}
 
-export const ProductDynamicSection = memo(function ProductDynamicSection() {
-  const { product: productAny } = useProduct();
-  const product = productAny as ProductFragment;
+function StoneSection() {
+  const { product } = useProduct();
+  return (
+    <div className="flex items-center gap-6 divide-x divide-black">
+      <StoneSpecifications
+        className="flex gap-9"
+        specifications={product.stoneSpecifications}
+      />
+      {product.stoneCertificate ? (
+        <StoneCertificate className="pl-6" image={product.stoneCertificate} />
+      ) : null}
+    </div>
+  );
+}
+
+export function ProductDynamicSection() {
+  const { product } = useProduct();
 
   if (product.productType === PRODUCT_TYPES.Setting) {
     return (
@@ -146,5 +182,9 @@ export const ProductDynamicSection = memo(function ProductDynamicSection() {
     );
   }
 
+  if (product.productType === PRODUCT_TYPES.Stone) {
+    return <StoneSection />;
+  }
+
   return <ProductCTA />;
-});
+}
