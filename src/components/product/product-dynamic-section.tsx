188ca1a4706e61fb10695483a71fd7d05ab612f4
jovan <jovanus@yahoo.co.id>
2024-08-19 23:09:21 +0700

    fix: useLocalStorage is not defined at client

--- src/components/product/product-dynamic-section.tsx
+++ src/components/product/product-dynamic-section.tsx
@@ -5,8 +5,17 @@ import { useProduct } from "@/app/product/provider";
 import { StoneModalProvider } from "@/providers/stone-modal-provider";
 
 import { ProductCTA } from "@/components/product/product-cta";
-import { ProductSettingSection } from "@/components/product/setting/product-setting-section";
 import { PRODUCT_TYPES } from "@/lib/constant";
+
+const ProductSettingSection = dynamic(
+  () =>
+    import("@/components/product/setting/product-setting-section").then(
+      (m) => m.ProductSettingSection,
+    ),
+  {
+    ssr: false,
+  },
+);
 
 const StoneSection = dynamic(() =>
   import("@/components/product/stone/product-stone-section").then(
