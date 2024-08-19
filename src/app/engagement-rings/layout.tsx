f7fbbb15c9d017faf1f7a115e663a5fdf9c7e416
jovan <jovanus@yahoo.co.id>
2024-08-19 22:20:14 +0700

    chore: remove unused component / page

--- src/app/engagement-rings/layout.tsx
+++ src/app/engagement-rings/layout.tsx
@@ -1,8 +1,6 @@
 import { ReactNode, Suspense } from "react";
 import { Metadata } from "next";
 import BuildStepper from "@/app/engagement-rings/build-stepper";
-import EngagementRingsHero from "@/app/engagement-rings/engagement-rings-hero";
-import { ProductModalProvider } from "@/app/engagement-rings/product-modal-provider";
 import { RingBuilderProvider } from "@/app/engagement-rings/ring-builder-provider";
 
 export const metadata: Metadata = {
@@ -15,17 +13,14 @@ export default function EngagementRingsLayout({
   children: ReactNode;
 }) {
   return (
-    <ProductModalProvider>
-      <RingBuilderProvider>
-        <div className="flex flex-col gap-6 pb-12">
-          <EngagementRingsHero />
-          <Suspense>
-            <BuildStepper />
-          </Suspense>
+    <RingBuilderProvider>
+      <div className="flex flex-col gap-6 pb-12">
+        <Suspense>
+          <BuildStepper />
+        </Suspense>
 
-          {children}
-        </div>
-      </RingBuilderProvider>
-    </ProductModalProvider>
+        {children}
+      </div>
+    </RingBuilderProvider>
   );
 }
