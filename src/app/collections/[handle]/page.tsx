96844c435c7b7498c5558d5ab9e97679bd431efd
jovan <jovanus@yahoo.co.id>
2024-08-19 22:29:44 +0700

    feat: collection hero art direction

--- src/app/collections/[handle]/page.tsx
+++ src/app/collections/[handle]/page.tsx
@@ -1,12 +1,9 @@
-import { Suspense } from "react";
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
-import { Image } from "@nextui-org/image";
 
+import CollectionHero from "@/components/collection/collection-hero";
 import { CollectionProducts } from "@/components/collection/collection-products";
-import { subtitle, title as titleClassName } from "@/components/primitives";
 import { getCollection } from "@/lib/shopify";
-import { generateSrcSet } from "@/lib/utils";
 
 export async function generateMetadata({
   params,
@@ -34,30 +31,19 @@ export default async function Page({ params }: { params: { handle: string } }) {
 
   return (
     <div className="flex flex-col gap-6 pb-12">
-      {/*  TODO: art direction */}
+      <CollectionHero
+        desktop_file={collection.desktop_media?.reference}
+        mobile_file={collection.mobile_media?.reference}
+      />
 
-      {collection.desktop_media?.reference?.__typename === "MediaImage" ? (
-        <Image
-          width="100%"
-          height={400}
-          classNames={{
-            img: "object-cover",
-          }}
-          srcSet={generateSrcSet(collection.desktop_media.reference.image?.url)}
-          src={collection.desktop_media.reference.image?.url}
-          sizes="100vw"
-          radius="none"
-        />
-      ) : null}
-
-      <dl className="container">
-        <dt className={titleClassName({ class: "capitalize" })}>
-          {collection.title}
-        </dt>
-        <dd className={subtitle({ className: "line-clamp-3 md:line-clamp-4" })}>
-          {collection.description}
-        </dd>
-      </dl>
+      {/*<dl className="container">*/}
+      {/*  <dt className={titleClassName({ class: "capitalize" })}>*/}
+      {/*    {collection.title}*/}
+      {/*  </dt>*/}
+      {/*  <dd className={subtitle({ className: "line-clamp-3 md:line-clamp-4" })}>*/}
+      {/*    {collection.description}*/}
+      {/*  </dd>*/}
+      {/*</dl>*/}
 
       <CollectionProducts collection={collection} />
     </div>
