5f3d2f36229a2c63b39598a87211ed6f1ebb1ae3
jovan <jovanus@yahoo.co.id>
2024-09-15 16:39:47 +0700

    refactor: extract article-card

--- src/app/blogs/[blog]/page.tsx
+++ src/app/blogs/[blog]/page.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from "next";
 import NextLink from "next/link";
-import { ArticleFragment } from "@/__generated__/graphql";
-import { Card, CardBody } from "@nextui-org/card";
-import { Image } from "@nextui-org/image";
 
+import { ArticleCard } from "@/components/blog/article-card";
 import { title as titleStyle } from "@/components/primitives";
 import { getBlog, getBlogs } from "@/lib/shopify";
-import { generateSrcSet } from "@/lib/utils";
 
 export const revalidate = 30;
 export const dynamicParams = true;
@@ -29,35 +26,6 @@ export async function generateMetadata({
   };
 }
 
-const ArticleCard = ({ article }: { article: ArticleFragment }) => {
-  return (
-    <Card as="figure" radius="none" shadow="none">
-      <Image
-        radius="none"
-        classNames={{
-          img: "aspect-video md:aspect-square object-cover object-center",
-        }}
-        alt={article.image?.altText || article.title}
-        src={article.image?.url}
-        srcSet={article.image?.url ? generateSrcSet(article.image?.url) : ""}
-        sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw"
-      />
-      <CardBody as="figcaption" className="px-0">
-        <h2
-          aria-label="article title"
-          className="line-clamp-1 text-lg font-medium"
-          title={article.title}
-        >
-          {article.title}
-        </h2>
-        <p className="line-clamp-3 font-light text-default-600">
-          {article.excerpt}
-        </p>
-      </CardBody>
-    </Card>
-  );
-};
-
 export default async function BlogPage({
   params,
 }: {
@@ -71,9 +39,10 @@ export default async function BlogPage({
       <p className="mb-9 mt-1.5 text-balance text-default-700">
         {seo?.description}
       </p>
-      <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+      <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {articles.map((article) => (
           <NextLink
+            prefetch
             key={article.id}
             href={`/blogs/${handle}/${article.handle}`}
             className="block h-full p-1"
---
+++ src/components/blog/article-card.tsx
@@ -0,0 +1,33 @@
+import { ArticleFragment } from "@/__generated__/graphql";
+import { Card, CardBody } from "@nextui-org/card";
+import { Image } from "@nextui-org/image";
+
+import { generateSrcSet } from "@/lib/utils";
+
+export const ArticleCard = ({ article }: { article: ArticleFragment }) => {
+  return (
+    <Card as="figure" radius="none" shadow="none">
+      <Image
+        radius="none"
+        classNames={{
+          img: "aspect-square object-cover object-center w-full",
+          wrapper: "!max-w-none",
+        }}
+        alt={article.image?.altText || article.title}
+        src={article.image?.url}
+        srcSet={article.image?.url ? generateSrcSet(article.image?.url) : ""}
+        sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw"
+      />
+      <CardBody as="figcaption" className="gap-2 px-0 py-4">
+        <h2
+          aria-label="article title"
+          className="text-2xl font-medium"
+          title={article.title}
+        >
+          {article.title}
+        </h2>
+        <p className="font-light text-default-600">{article.excerpt}</p>
+      </CardBody>
+    </Card>
+  );
+};
