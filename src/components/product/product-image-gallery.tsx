fba2aa7e17494fa4215e517cab6f1ff36edfeb4c
jovan <jovanus@yahoo.co.id>
2024-09-24 17:25:45 +0700

    style: revamp image gallery on desktop view

--- src/components/product/product-image-gallery.tsx
+++ src/components/product/product-image-gallery.tsx
@@ -69,95 +69,116 @@ export default function ProductImageGallery({
   }, [api, onSelect]);
 
   return (
-    <div className="flex flex-col gap-3 md:px-3">
-      <div className="relative" role="region" aria-roledescription="carousel">
-        <div ref={carouselRef} className="overflow-hidden">
-          <div className={cn("-ml-4 flex")}>
-            {images.map((image, index) => (
-              <div
-                key={index}
-                onClick={() => scrollTo(index)}
-                role="group"
-                aria-roledescription="slide"
-                className="min-w-0 shrink-0 grow-0 basis-full pl-4"
-              >
-                <Image
-                  decoding="async"
-                  loading="lazy"
-                  radius="none"
-                  classNames={{
-                    wrapper: "aspect-square !max-w-none",
-                  }}
-                  className="h-full w-full object-cover object-center"
-                  src={image.url}
-                  srcSet={generateSrcSet(image.url)}
-                  alt={image.altText || ""}
-                  sizes="(max-width: 768px) 100vw, (max-width:1536px): 50vw"
-                />
-              </div>
-            ))}
+    <>
+      <div className="grid grid-cols-2 gap-3 max-lg:hidden">
+        {images.map((image, index) => (
+          <Image
+            loading="lazy"
+            decoding="async"
+            radius="none"
+            draggable={false}
+            key={index}
+            src={image.url}
+            srcSet={generateSrcSet(image.url)}
+            sizes="(min-width: 1024px) 34vw"
+            alt={image.altText || ""}
+            classNames={{
+              wrapper: "aspect-square !max-w-none",
+            }}
+            className="h-full w-full object-cover object-center"
+          />
+        ))}
+      </div>
+      <div className="flex flex-col gap-3 md:px-3 lg:hidden">
+        <div className="relative" role="region" aria-roledescription="carousel">
+          <div ref={carouselRef} className="overflow-hidden">
+            <div className={cn("-ml-4 flex")}>
+              {images.map((image, index) => (
+                <div
+                  key={index}
+                  onClick={() => scrollTo(index)}
+                  role="group"
+                  aria-roledescription="slide"
+                  className="min-w-0 shrink-0 grow-0 basis-full pl-4"
+                >
+                  <Image
+                    decoding="async"
+                    loading="lazy"
+                    radius="none"
+                    classNames={{
+                      wrapper: "aspect-square !max-w-none",
+                    }}
+                    className="h-full w-full object-cover object-center"
+                    src={image.url}
+                    srcSet={generateSrcSet(image.url)}
+                    alt={image.altText || ""}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1536px) 50vw"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="absolute bottom-0 right-4 flex -translate-y-1/2 items-center gap-3">
+            <Button
+              isIconOnly
+              variant="flat"
+              className={"size-8 rounded-full"}
+              disabled={!canScrollPrev}
+              onClick={scrollPrev}
+            >
+              <ArrowLeftIcon className="size-4" />
+              <span className="sr-only">Previous slide</span>
+            </Button>
+            <Button
+              isIconOnly
+              variant="flat"
+              className={"size-8 rounded-full"}
+              disabled={!canScrollNext}
+              onClick={scrollNext}
+            >
+              <ArrowRightIcon className="size-4" />
+              <span className="sr-only">Previous slide</span>
+            </Button>
           </div>
         </div>
 
-        <div className="absolute bottom-0 right-4 flex -translate-y-1/2 items-center gap-3">
-          <Button
-            isIconOnly
-            variant="flat"
-            className={"size-8 rounded-full"}
-            disabled={!canScrollPrev}
-            onClick={scrollPrev}
-          >
-            <ArrowLeftIcon className="size-4" />
-            <span className="sr-only">Previous slide</span>
-          </Button>
-          <Button
-            isIconOnly
-            variant="flat"
-            className={"size-8 rounded-full"}
-            disabled={!canScrollNext}
-            onClick={scrollNext}
-          >
-            <ArrowRightIcon className="size-4" />
-            <span className="sr-only">Previous slide</span>
-          </Button>
-        </div>
-      </div>
-
-      <div className="relative" role="region" aria-roledescription="carousel">
-        <div ref={thumbRef} className="overflow-hidden max-md:px-3">
-          <div className={cn("-ml-3 flex")}>
-            {images.map((image, index) => (
-              <div
-                key={index}
-                onClick={() => scrollTo(index)}
-                role="group"
-                aria-roledescription="slide"
-                className={cn(
-                  "min-w-0 shrink-0 grow-0 basis-1/3 pl-3 md:basis-1/4 lg:basis-1/5",
-                )}
-              >
-                <Image
-                  loading="lazy"
-                  decoding="async"
-                  radius="none"
-                  classNames={{
-                    wrapper: cn(
-                      "relative aspect-square h-full w-full !max-w-none",
-                      selectedIndex === index && "border border-primary",
-                    ),
-                  }}
-                  className="h-full w-full object-cover object-center"
-                  src={image.url}
-                  alt={image.altText || ""}
-                  sizes="(max-width: 768px) 100vw, 50vw"
-                  // sizes="(max-width: 768px) 33vw, (max-width: 1024px) 15vw, 10vw"
-                  // srcSet={generateSrcSet(image.url)}
-                />
-              </div>
-            ))}
+        <div className="relative" role="region" aria-roledescription="carousel">
+          <div ref={thumbRef} className="overflow-hidden max-md:px-3">
+            <div className={cn("-ml-3 flex")}>
+              {images.map((image, index) => (
+                <div
+                  key={index}
+                  onClick={() => scrollTo(index)}
+                  role="group"
+                  aria-roledescription="slide"
+                  className={cn(
+                    "min-w-0 shrink-0 grow-0 basis-1/3 pl-3 md:basis-1/4 lg:basis-1/5",
+                  )}
+                >
+                  <Image
+                    loading="lazy"
+                    decoding="async"
+                    radius="none"
+                    classNames={{
+                      wrapper: cn(
+                        "relative aspect-square h-full w-full !max-w-none",
+                        selectedIndex === index && "border border-primary",
+                      ),
+                    }}
+                    className="h-full w-full object-cover object-center"
+                    src={image.url}
+                    alt={image.altText || ""}
+                    sizes="(max-width: 768px) 100vw, 50vw"
+                    // sizes="(max-width: 768px) 33vw, (max-width: 1024px) 15vw, 10vw"
+                    // srcSet={generateSrcSet(image.url)}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
