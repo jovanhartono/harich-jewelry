205e8635f79fa76de10fb74b99173c1888334e80
jovan <jovanus@yahoo.co.id>
2024-09-27 01:47:52 +0700

    chore: adjust siteConfig

--- src/config/site.ts
+++ src/config/site.ts
@@ -7,12 +7,10 @@ export const siteConfig = {
   url: "https://harich-jewelry.vercel.app",
   description: "Timeless Elegance – Fine Jewelry for Every Occasion",
   email: "mail@goosh.id",
-  phone: "+6282318270480",
+  phone: "+6281908195688",
   links: {
-    condition_guide: "/pages/item-condition-guide",
-    policy: "/pages/quality-control-return-policy",
-    whatsapp: `https://api.whatsapp.com/send?phone=6282318270480&text=${encodeURIComponent("Hi Goosh, I want to ask about...")}&type=phone_number`,
-    instagram: "https://www.instagram.com/goosh_id/",
-    tiktok: "https://tiktok.com",
+    whatsapp: `https://api.whatsapp.com/send?phone=6281908195688`,
+    instagram: "https://www.instagram.com/harichjewelry",
+    tiktok: "https://www.tiktok.com/@harichjewelry",
   },
 };
