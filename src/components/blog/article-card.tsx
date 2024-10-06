467a84922349af3ae867b524bd5c43b0e9cf47e3
jovan <jovanus@yahoo.co.id>
2024-10-06 15:26:25 +0700

    refactor(article card): use next image instead

--- src/components/blog/article-card.tsx
+++ src/components/blog/article-card.tsx
@@ -1,21 +1,16 @@
 import NextImage from "next/image";
 import { ArticleFragment } from "@/__generated__/graphql";
 import { Card, CardBody } from "@nextui-org/card";
-import { Image } from "@nextui-org/image";
 
 export const ArticleCard = ({ article }: { article: ArticleFragment }) => {
   return (
     <Card as="figure" radius="none" shadow="none">
-      <Image
-        as={NextImage}
-        radius="none"
-        classNames={{
-          img: "object-cover object-center w-full h-full",
-          wrapper: "!max-w-none relative aspect-square",
-        }}
-        fill
+      <NextImage
+        width={500}
+        height={500}
         alt={article.image?.altText || article.title}
         src={article.image?.url}
+        className="aspect-square object-cover object-center"
         sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
       />
       <CardBody as="figcaption" className="gap-2 px-0 py-4">
