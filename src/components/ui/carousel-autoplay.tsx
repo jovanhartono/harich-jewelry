5a5ecb41374ef26e28b6853ed496d1e5fbf908ac
jovan <jovanus@yahoo.co.id>
2024-10-10 15:08:34 +0700

    chore(carousel-autoplay): add className props

--- src/components/ui/carousel-autoplay.tsx
+++ src/components/ui/carousel-autoplay.tsx
@@ -4,17 +4,21 @@ import { ReactNode } from "react";
 import Autoplay from "embla-carousel-autoplay";
 
 import { Carousel } from "@/components/ui/carousel";
+import { cn } from "@/lib/utils";
 
 export default function CarouselAutoplay({
   children,
+  className,
 }: {
   children: ReactNode;
+  className?: string;
 }) {
   return (
     <Carousel
-      className="mx-auto w-full"
+      className={cn("mx-auto w-full", className)}
       opts={{
         loop: true,
+        align: "start",
       }}
       plugins={[
         Autoplay({
