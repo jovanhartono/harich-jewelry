968ad9a4daf5e2c484f06bef9137c0a30a3d49f6
jovan <jovanus@yahoo.co.id>
2025-01-03 20:50:02 +0700

    style: adjust navbar width

--- src/components/layout/navbar/navbar-wrapper.tsx
+++ src/components/layout/navbar/navbar-wrapper.tsx
@@ -17,18 +17,18 @@ export const NavbarWrapper = ({ children }: { children: ReactNode }) => {
 
   return (
     <NextUINavbar
-      maxWidth="full"
+      maxWidth="2xl"
       isMenuOpen={isMenuOpen}
       onMenuOpenChange={setIsMenuOpen}
       height="5rem"
       id="harich-header"
       isBlurred={false}
       classNames={{
-        base: "z-50 bg-background",
-        wrapper: "w-full bg-background z-50 px-0",
+        base: "z-50 bg-background border-b border-b-default-500",
+        wrapper: "w-full bg-background z-50 px-0 ",
       }}
     >
-      <div className="container relative z-50 flex h-[var(--navbar-height)] w-full max-w-full flex-row flex-nowrap items-center justify-between gap-4 border-b border-b-default-500 bg-background">
+      <div className="container relative z-50 flex h-[var(--navbar-height)] w-full max-w-full flex-row flex-nowrap items-center justify-between gap-4">
         {children}
       </div>
     </NextUINavbar>
